077461b704056daad3d7dad77c153281a2418df8
TatoSec <Ivang.fonsecafrias@gmail.com>
2023-04-22 16:18:29 -0400

    Bare Bones Url Scanner

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from api_key import virus_total_key
 
 client = vt.Client(virus_total_key)
 
+
+
+# URL SCANNER
+url_id = vt.url_id("http://google.com")
+url = client.get_object("/urls/{}", url_id)
+
+url = client.get_object("/urls/{}".format(url_id))
+
+#print(url.last_analysis_stats)  #print function allows us to enumerate results
+
+
