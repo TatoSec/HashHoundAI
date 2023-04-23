3596d401ec1b7ebbd3f48101ddf364fcb30a0182
TatoSec <Ivang.fonsecafrias@gmail.com>
2023-04-23 10:13:07 -0400

    Extracting Desired Data

--- scanner.py
+++ scanner.py
@@ -2,6 +2,7 @@ import vt
 import os
 import openai
 import requests
+import json
 from api_key import virus_total_key
 from api_key import openai_key
 
@@ -9,7 +10,7 @@ from api_key import openai_key
 # Activating API 
 client = vt.Client(virus_total_key)
 
-# Hash Analysis
+# Get a Report by Hash
 url = "https://www.virustotal.com/api/v3/files/db3f663417baec4d8da89267a4a27df5"
 
 
@@ -20,4 +21,18 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-#print(response.text)
+data = response.text
+
+parsed_data = json.loads(data)
+
+type_description = parsed_data['data']['attributes']['type_description']
+
+
+
+print(type_description)
+
+
+
+
+
+
