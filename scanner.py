4582cb9fb446f6005ac7bf4f491429b391494ce2
TatoSec <Ivang.fonsecafrias@gmail.com>
2023-04-23 11:04:10 -0400

    Edited Extraction

--- scanner.py
+++ scanner.py
@@ -26,10 +26,22 @@ data = response.text
 
 parsed_data = json.loads(data)
 
+#Subheaders in JSON File
 type_description = parsed_data['data']['attributes']['type_description']
 classification = parsed_data['data']['attributes']['popular_threat_classification']['suggested_threat_label']
 times_submitted = parsed_data['data']['attributes']['times_submitted']
 file_size = parsed_data['data']['attributes']["size"]
+last_analysis_results = parsed_data['data']['attributes']['last_analysis_results']
+aliases = parsed_data['data']['attributes']['names']
+
+categories = []
+engine_names = []
+result = []
+for engine in last_analysis_results.values():
+    categories.append(engine['category'])
+    engine_names.append(engine['engine_name'])
+    result.append(engine['result'])
+
 
 
 #print(response.text)
@@ -37,6 +49,14 @@ print(f"Type: {type_description}")
 print(f"classification: {classification}")
 print(f"File Size: {file_size}mb")
 print(f"Submissions: {times_submitted}")
+print(f"Aliases: {aliases}")
+
+
+# Enumerate Total Analysis
+#print(f"AV-Analysis: {categories, engine_names, result}")
+
+
+
 
 
 
