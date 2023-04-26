ebe6ae8277765dc969dd68f4df2ad8990e0501eb
TatoSec <Ivang.fonsecafrias@gmail.com>
2023-04-26 14:29:53 -0400

    detect and elaborate on benign Hashes

--- hash-scanner.py
+++ hash-scanner.py
@@ -30,7 +30,13 @@ parsed_data = json.loads(data)
 
 #Subheaders in JSON File
 type_description = parsed_data['data']['attributes']['type_description']
-classification = parsed_data['data']['attributes']['popular_threat_classification']['suggested_threat_label']
+classification = parsed_data
+if 'popular_threat_classification' in parsed_data['data']['attributes']:
+    classification = parsed_data['data']['attributes']['popular_threat_classification']['suggested_threat_label']
+
+elif 'popular_threat_classification' not in parsed_data['data']['attributes']:
+   classification = "No Description"
+
 times_submitted = parsed_data['data']['attributes']['times_submitted']
 file_size = parsed_data['data']['attributes']["size"]
 last_analysis_results = parsed_data['data']['attributes']['last_analysis_results']
@@ -58,14 +64,24 @@ for engine in last_analysis_results.values():
 # OpenAI Integration
 openai.api_key = openai_key
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "Your name is HashHoundAI you are a helpful cybersecurity assistant that knows everthing related to files and hashes"},
-    {"role": "user", "content": f"Go in depth about this hash and what an attacker cna achieve wiht this, can you also organize last_analysis_results such as egnine:status:name{hash,type_description,aliases,last_analysis_results}"},
-    {"role": "user", "content": "if the file is malicioustell me how to remediate it if its already in my system"}
-  ]
-)
+if classification == "No Description":
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "Your name is HashHoundAI you are a helpful cybersecurity assistant that knows everthing related to files and hashes"},
+      {"role": "user", "content": f"determine weather the hash is malicious or not and Give an in depth explanation about the hash provided and what it does given this data:{hash,type_description}"},
+    ]
+  )
+
+else:
+   
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "Your name is HashHoundAI you are a helpful cybersecurity assistant that knows everthing related to files and hashes"},
+      {"role": "user", "content": f"Go in depth about this hash, determin wether its malicious and what an attacker can achieve given this data:{hash,classification,type_description,aliases,last_analysis_results}. now give remediation"},
+    ]
+  )
 
 
 
@@ -79,8 +95,3 @@ def hash_scan ():
 
 
 hash_scan()
-
-
-
-
-
